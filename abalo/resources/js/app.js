bf57dc0d23746194dc281b11d36771c825a136be
Phuong <phuong.nguyen@alumni.fh-aachen.de>
2025-05-22 16:34:31 +0200

    try vite

--- abalo/resources/js/app.js
+++ abalo/resources/js/app.js
@@ -1,4 +1,17 @@
 import './bootstrap';
 import {pi, round} from 'mathjs';
+import {createApp} from 'vue';
 
+const app = createApp({
+    data(){
+        return {
+            msg: "Hallo Welt",
+        }
+    }
+}).mount('#app');
+
+
+//
 alert(round(pi, 2));
+
+
